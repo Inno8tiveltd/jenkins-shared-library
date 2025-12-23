dbdd122f2e64a7216f5b974bc0742ed16996622b
skfareed-dev <fsk127699@gmail.com>
2025-12-23 22:25:15 +0530

    changes

--- vars/deployPipeline.groovy
+++ vars/deployPipeline.groovy
@@ -1,8 +1,7 @@
 def call() {
     def branch = env.BRANCH_NAME
     echo "Deploying for branch: ${branch}"
-
-    // Wrap all string credentials here
+    
     withCredentials([
         string(credentialsId: 'DEV_SERVER_IP', variable: 'DEV_IP'),
         string(credentialsId: 'STAGE_SERVER_IP', variable: 'STAGE_IP'),
@@ -15,19 +14,26 @@ def call() {
         if (branch == 'dev') {
             serverIP = DEV_IP
             envFile = '.env.dev'
-        } else if (branch == 'stage') {
+        }
+        else if (branch == 'stage') {
             serverIP = STAGE_IP
             envFile = '.env.stage'
-        } else if (branch == 'main' || branch == 'prod') {
+        }
+        else if (branch == 'main' || branch == 'prod') {
             serverIP = PROD_IP
             envFile = '.env.prod'
-        } else {
+        }
+        else {
             echo "No deployment for branch: ${branch}"
             return
         }
 
         echo "Deploying branch ${branch} to ${serverIP}"
 
-
+        // 🔥 THIS WAS MISSING
+        deploy(
+            server: "ec2-user@${serverIP}",
+            envFile: envFile
+        )
     }
 }
