42138d52ee2865815eba935957f0e06fb1a95cf5
skfareed-dev <fsk127699@gmail.com>
2025-12-23 22:34:27 +0530

    changes

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,12 +1,10 @@
-def call(Map config) {
-
-    sshagent(credentials: ['ec2-ssh-key']) {
-        sh """
-        ssh -o StrictHostKeyChecking=no ${config.server} '
-            cd /home/ec2-user/node-app &&
-            docker compose --env-file ${config.envFile} pull &&
-            docker compose --env-file ${config.envFile} up -d
-        '
-        """
-    }
+sshagent(credentials: ['ec2-ssh-key']) {
+    sh """
+    ssh -o StrictHostKeyChecking=no ec2-user@${serverIP} '
+        echo "${NEXUS_PASSWORD}" | docker login 13.251.194.219:8083 -u ${NEXUS_USER} --password-stdin &&
+        cd /home/ec2-user/node-app &&
+        docker compose --env-file ${envFile} pull &&
+        docker compose --env-file ${envFile} up -d
+    '
+    """
 }
