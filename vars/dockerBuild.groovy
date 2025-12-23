8f5d1a227b15dd2976e33a17d55c386b08f65f38
skfareed-dev <fsk127699@gmail.com>
2025-12-23 21:41:19 +0530

    added user-portal-frontend

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,9 +1,14 @@
-def call(steps, String backend, String frontend, String tag) {
-    steps.stage('Build Backend Docker Image') {
+def call(steps, backend, frontend, userFrontend, tag) {
+
+    steps.stage('Build Backend') {
         steps.sh "docker build -t ${backend}:${tag} backend"
     }
 
-    steps.stage('Build Frontend Docker Image') {
+    steps.stage('Build Frontend') {
         steps.sh "docker build -t ${frontend}:${tag} frontend"
+    }
+
+    steps.stage('Build User Frontend') {
+        steps.sh "docker build -t ${userFrontend}:${tag} user-portal-frontend"
     }
 }
