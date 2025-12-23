9317a8b9d06f2bb1ebdc66da6ae77be0cb9057c5
skfareed-dev <fsk127699@gmail.com>
2025-12-23 22:36:22 +0530

    changes

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,10 +1,19 @@
-sshagent(credentials: ['ec2-ssh-key']) {
-    sh """
-    ssh -o StrictHostKeyChecking=no ec2-user@${serverIP} '
-        echo "${NEXUS_PASSWORD}" | docker login 13.251.194.219:8083 -u ${NEXUS_USER} --password-stdin &&
-        cd /home/ec2-user/node-app &&
-        docker compose --env-file ${envFile} pull &&
-        docker compose --env-file ${envFile} up -d
-    '
-    """
+def call(Map config) {
+    sshagent(credentials: ['ec2-ssh-key']) {
+        withCredentials([usernamePassword(
+            credentialsId: 'nexus-docker-creds',
+            usernameVariable: 'NEXUS_USER',
+            passwordVariable: 'NEXUS_PASSWORD'
+        )]) {
+            sh """
+            ssh -o StrictHostKeyChecking=no ${config.server} '
+                # Login to Nexus Docker registry
+                echo "$NEXUS_PASSWORD" | docker login ${config.registry} -u $NEXUS_USER --password-stdin
+                cd /home/ec2-user/node-app
+                docker compose --env-file ${config.envFile} pull
+                docker compose --env-file ${config.envFile} up -d
+            '
+            """
+        }
+    }
 }
