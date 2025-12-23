4b707d507351984497e6edf0ead984d7dec94d98
skfareed-dev <fsk127699@gmail.com>
2025-12-23 20:23:00 +0530

    coresponding jfiles

--- vars/deployPipeline.groovy
+++ vars/deployPipeline.groovy
@@ -1,30 +1,42 @@
 def call() {
-
     def branch = env.BRANCH_NAME
     echo "Deploying for branch: ${branch}"
 
-    if (branch == 'dev') {
-        deploy(
-            envName: 'dev',
-            server: 'ec2-user@DEV_SERVER_IP',
-            envFile: '.env.dev'
-        )
-    }
-    else if (branch == 'stage') {
-        deploy(
-            envName: 'stage',
-            server: 'ec2-user@STAGE_SERVER_IP',
-            envFile: '.env.stage'
-        )
-    }
-    else if (branch == 'main' || branch == 'prod') {
-        deploy(
-            envName: 'prod',
-            server: 'ec2-user@PROD_SERVER_IP',
-            envFile: '.env.prod'
-        )
-    }
-    else {
-        echo "No deployment for branch: ${branch}"
+    // Wrap all string credentials here
+    withCredentials([
+        string(credentialsId: 'DEV_SERVER_IP', variable: 'DEV_IP'),
+        string(credentialsId: 'STAGE_SERVER_IP', variable: 'STAGE_IP'),
+        string(credentialsId: 'PROD_SERVER_IP', variable: 'PROD_IP')
+    ]) {
+
+        def serverIP = ''
+        def envFile = ''
+
+        if (branch == 'dev') {
+            serverIP = DEV_IP
+            envFile = '.env.dev'
+        } else if (branch == 'stage') {
+            serverIP = STAGE_IP
+            envFile = '.env.stage'
+        } else if (branch == 'main' || branch == 'prod') {
+            serverIP = PROD_IP
+            envFile = '.env.prod'
+        } else {
+            echo "No deployment for branch: ${branch}"
+            return
+        }
+
+        echo "Deploying branch ${branch} to ${serverIP}"
+
+        // SSH agent with your PEM key
+        sshagent(credentials: ['ec2-ssh-key']) {
+            sh """
+            ssh -o StrictHostKeyChecking=no ec2-user@${serverIP} '
+                cd /home/ec2-user/node-app &&
+                docker compose --env-file ${envFile} pull &&
+                docker compose --env-file ${envFile} up -d
+            '
+            """
+        }
     }
 }
