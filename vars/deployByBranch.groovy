59d7a735ceb3ad94abd089adfd1497855c7b1ccd
skfareed-dev <fsk127699@gmail.com>
2025-12-28 17:19:53 +0530

    changes

--- vars/deployByBranch.groovy
+++ vars/deployByBranch.groovy
@@ -1,12 +1,12 @@
 def call() {
     if (env.BRANCH_NAME == 'dev') {
-        return "dev-${env.BUILD_NUMBER}"
+        return 'DEV_SERVER_IP'
     }
     if (env.BRANCH_NAME == 'stage') {
-        return "stage-${env.BUILD_NUMBER}"
+        return 'STAGE_SERVER_IP'
     }
     if (env.BRANCH_NAME == 'prod') {
-        return "prod-${env.GIT_COMMIT.take(7)}"
+        return 'PROD_SERVER_IP'
     }
     error "Unsupported branch: ${env.BRANCH_NAME}"
 }
--- vars/deployPipeline.groovy
+++ vars/deployPipeline.groovy
@@ -1,10 +1,15 @@
 def call() {
     def tag = deployByBranch()
+    def serverCredId = resolveTargetServer()
 
-    deploy(
-        server: "ec2-user@${env.TARGET_SERVER}",
-        registry: "13.251.194.219:8083",
-        envFile: ".env.${env.ENV}",
-        imageTag: tag
-    )
+    withCredentials([
+        string(credentialsId: serverCredId, variable: 'TARGET_SERVER')
+    ]) {
+        deploy(
+            server: "ec2-user@${TARGET_SERVER}",
+            registry: "13.251.194.219:8083",
+            envFile: ".env.${env.BRANCH_NAME}",
+            imageTag: tag
+        )
+    }
 }
