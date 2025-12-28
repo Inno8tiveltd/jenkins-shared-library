8897df8359e22f1410ad9a729f64ff9b37407969
skfareed-dev <fsk127699@gmail.com>
2025-12-28 17:53:21 +0530

    changes

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,25 +1,14 @@
 def call(Map cfg) {
-    withCredentials([
-        string(credentialsId: env.SERVER_CRED, variable: 'SERVER_IP'),
-        usernamePassword(
-            credentialsId: 'nexus-docker-creds',
-            usernameVariable: 'NEXUS_USER',
-            passwordVariable: 'NEXUS_PASS'
-        )
-    ]) {
-        sshagent(credentials: ['ec2-ssh-key']) {
-            sh """
-            ssh -o StrictHostKeyChecking=no ec2-user@${SERVER_IP} '
-                echo "TAG=${cfg.imageTag}" > /home/ec2-user/node-app/.env.runtime
-                cat ${cfg.envFile} >> /home/ec2-user/node-app/.env.runtime
+    sh """
+    ssh -o StrictHostKeyChecking=no ${cfg.server} '
+        echo "TAG=${cfg.imageTag}" > /home/ec2-user/node-app/.env.runtime
+        cat ${cfg.envFile} >> /home/ec2-user/node-app/.env.runtime
 
-                docker login ${cfg.registry} -u ${NEXUS_USER} -p ${NEXUS_PASS}
+        docker login ${cfg.registry} -u ${cfg.nexusUser} -p ${cfg.nexusPass}
 
-                cd /home/ec2-user/node-app
-                docker compose --env-file .env.runtime pull
-                docker compose --env-file .env.runtime up -d
-            '
-            """
-        }
-    }
+        cd /home/ec2-user/node-app
+        docker compose --env-file .env.runtime pull
+        docker compose --env-file .env.runtime up -d
+    '
+    """
 }
--- vars/deployPipeline.groovy
+++ vars/deployPipeline.groovy
@@ -1,19 +1,32 @@
 def call() {
     echo "🚀 Deploy triggered for branch: ${env.BRANCH_NAME}"
 
+    // Get image tag & server credential ID
     def tag = deployByBranch()
     def serverCredId = serverCredByBranch()
 
     echo "🔐 Using credential ID: ${serverCredId}"
 
+    // Bind all credentials here
     withCredentials([
-        string(credentialsId: serverCredId, variable: 'TARGET_SERVER')
-    ]) {
-        deploy(
-            server: "ec2-user@${TARGET_SERVER}",
-            registry: "13.251.194.219:8083",
-            envFile: ".env.${env.BRANCH_NAME}",
-            imageTag: tag
+        string(credentialsId: serverCredId, variable: 'SERVER_IP'),
+        usernamePassword(
+            credentialsId: 'nexus-docker-creds',
+            usernameVariable: 'NEXUS_USER',
+            passwordVariable: 'NEXUS_PASS'
         )
+    ]) {
+        sshagent(credentials: ['ec2-ssh-key']) {
+            // Call deploy with all required info
+            deploy(
+                server: "ec2-user@${SERVER_IP}",
+                registry: "13.251.194.219:8083",
+                envFile: ".env.${env.BRANCH_NAME}",
+                imageTag: tag,
+                serverIp: SERVER_IP,
+                nexusUser: NEXUS_USER,
+                nexusPass: NEXUS_PASS
+            )
+        }
     }
 }
