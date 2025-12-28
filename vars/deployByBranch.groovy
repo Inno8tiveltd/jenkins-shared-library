db575cb72ede1c5c6f8734676b21cc02e813d765
skfareed-dev <fsk127699@gmail.com>
2025-12-28 17:13:17 +0530

    changes

--- vars/deployByBranch.groovy
+++ vars/deployByBranch.groovy
@@ -1,12 +1,12 @@
-def deployByBranch() {
+def call() {
     if (env.BRANCH_NAME == 'dev') {
-        return [ env: 'dev', serverCred: 'DEV_SERVER_IP', tag: "dev-${env.BUILD_NUMBER}" ]
+        return "dev-${env.BUILD_NUMBER}"
     }
     if (env.BRANCH_NAME == 'stage') {
-        return [ env: 'stage', serverCred: 'STAGE_SERVER_IP', tag: "stage-${env.BUILD_NUMBER}" ]
+        return "stage-${env.BUILD_NUMBER}"
     }
-    if (env.BRANCH_NAME == 'main') {
-        return [ env: 'prod', serverCred: 'PROD_SERVER_IP', tag: "prod-${env.BUILD_NUMBER}" ]
+    if (env.BRANCH_NAME == 'prod') {
+        return "prod-${env.GIT_COMMIT.take(7)}"
     }
     error "Unsupported branch: ${env.BRANCH_NAME}"
 }
